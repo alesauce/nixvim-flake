7e84997ed7f9dfe567bb6752beadec15b482fcc3
Alexander Sauceda <alesauce@amazon.com>
2023-11-07 21:38:18 -0800

    trying out function for jdtls config

--- config/lsp/nvim-jdtls.nix
+++ config/lsp/nvim-jdtls.nix
@@ -12,16 +12,18 @@
     initOptions = {
       workspaceFolders = {
         __raw = ''
-        local root_dir = require("jdtls.setup").find_root({ "packageInfo" }, "Config")
-        local ws_folders_jdtls = {}
-        if root_dir then
-         local file = io.open(root_dir .. "/.bemol/ws_root_folders")
-         if file then
-          for line in file:lines() do
-           table.insert(ws_folders_jdtls, "file://" .. line)
+        function ()
+          local root_dir = require("jdtls.setup").find_root({ "packageInfo" }, "Config")
+          local ws_folders_jdtls = {}
+          if root_dir then
+           local file = io.open(root_dir .. "/.bemol/ws_root_folders")
+           if file then
+            for line in file:lines() do
+             table.insert(ws_folders_jdtls, "file://" .. line)
+            end
+            file:close()
+           end
           end
-          file:close()
-         end
         end
         '';
       };
