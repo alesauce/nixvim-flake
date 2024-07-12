1ce1bc9c88ae4eb2a4114b6bd1cc5e19fdbb1a4a
Alexander Sauceda <alexander@alexandersauceda.dev>
2024-07-11 19:41:03 -0700

    removing ensureInstalled, setting nixGrammars to false and setting parser dir

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -2,5 +2,9 @@
   plugins.treesitter = {
     enable = true;
     nixvimInjections = true;
+    nixGrammars = true;
+    settings = {
+      parser_install_dir = "$XDG_DATA_HOME/nvim/treesitter";
+    };
   };
 }
