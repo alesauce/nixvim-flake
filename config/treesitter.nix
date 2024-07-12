ae3e18b593084620fa98c9ac7fc9f9406d1efc7d
Alexander Sauceda <alexander@alexandersauceda.dev>
2024-07-11 19:19:33 -0700

    removing all ensure_installed grammars

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -1,50 +1,6 @@
 {
   plugins.treesitter = {
     enable = true;
-
     nixvimInjections = true;
-
-    ensureInstalled = [
-      "bash"
-      "dockerfile"
-      "git_config"
-      "git_rebase"
-      "gitattributes"
-      "gitcommit"
-      "gitignore"
-      "go"
-      "gomod"
-      "gosum"
-      "gowork"
-      "graphql"
-      "haskell_persistent"
-      "java"
-      "javascript"
-      "jq"
-      "json"
-      "json5"
-      "lua"
-      "luadoc"
-      "lua patterns"
-      "luau"
-      "markdown"
-      "markdown_inline"
-      "nickel"
-      "nix"
-      "python"
-      "regex"
-      "rust"
-      "smithy"
-      "sql"
-      "terraform"
-      "todotxt"
-      "toml"
-      "tsx"
-      "typescript"
-      "vim"
-      "vimdoc"
-      "yaml"
-      "zig"
-    ];
   };
 }
