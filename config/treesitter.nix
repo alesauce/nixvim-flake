7c64d93fe853799858556f7ac15a186d4bfe196f
Alexander Sauceda <alexander@alexandersauceda.dev>
2024-07-12 21:54:29 -0600

    feat(config/treesitter) - Adding all parsers, cleaning up core, enabling highlight

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -5,6 +5,8 @@
     nixvimInjections = true;
     nixGrammars = true;
     settings = {
+      ensure_installed = ["all"];
+      hightlight.enable = true;
       indent = true;
       parser_install_dir = "$XDG_DATA_HOME/nvim/treesitter";
     };
