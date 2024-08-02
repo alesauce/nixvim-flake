5d3f055b1179a7e4ea5a8dd747bc085b6f72b128
Alexander Sauceda <alexander@alexandersauceda.dev>
2024-08-01 22:27:59 -0600

    feat(navigation/harpoon) - adding new keymaps for managing harpoon marks

--- config/navigation/harpoon.nix
+++ config/navigation/harpoon.nix
@@ -1,19 +1,17 @@
 {
-  # TODO: review docs and configure harpoon: https://github.com/ThePrimeagen/harpoon
-  # nixvim docs: https://nix-community.github.io/nixvim/#_plugins_harpoon_enable
   plugins.harpoon = {
     enable = true;
+    markBranch = true;
 
     keymaps = {
       addFile = "<leader>ha";
+      toggleQuickMenu = "<leader>hm";
       navFile = {
         "1" = "<C-l>";
         "2" = "<C-u>";
         "3" = "<C-y>";
         "4" = "<C-;>";
       };
-      # TODO: create tmuxp project sessions with standard tmux identifier for notes pane:
-      # https://nix-community.github.io/nixvim/plugins/harpoon/keymaps.html#pluginsharpoonkeymapstmuxgototerminal
     };
   };
 }
