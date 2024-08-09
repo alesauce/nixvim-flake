51f80aab4936bbdef7792d6e3f100954d0fba5bb
Alexander Sauceda <alexander@alexandersauceda.dev>
2024-08-09 17:43:42 -0600

    feat(lang/haskell) - Adding haskell-tools-nvim support
    Switching to use the wonderful [haskell-tools-nvim](https://github.com/mrcjkb/haskell-tools.nvim?tab=readme-ov-file) plugin for Haskell projects. This does not currently include debug adapter support - that will need to come in a future commit.
    
    Tested this change by:
    - `nix flake check --show-trace --all-systems`
    - `nix build`; Followed this by using the `result` on a local Haskell
      project.

--- config/lang/haskell.nix
+++ config/lang/haskell.nix
@@ -1,8 +1,36 @@
-{
+{pkgs, ...}: {
   config = {
-    plugins = {
-      haskell-scope-highlighting.enable = true;
-      lsp.servers.hls.enable = true;
+    plugins.haskell-scope-highlighting.enable = true;
+    extraPlugins = with pkgs.vimPlugins; [
+      # https://github.com/mrcjkb/haskell-tools.nvim
+      haskell-tools-nvim
+    ];
+    extraPackages = with pkgs.haskellPackages; [
+      fast-tags
+      hoogle
+      haskell-language-server
+    ];
+    files."ftplugin/haskell.lua" = {
+      extraConfigLua = ''
+        require('telescope').load_extension('ht')
+        local ht = require('haskell-tools')
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = { noremap = true, silent = true, buffer = bufnr, }
+        -- haskell-language-server relies heavily on codeLenses,
+        -- so auto-refresh (see advanced configuration) is enabled by default
+        vim.keymap.set('n', '<space>cl', vim.lsp.codelens.run, opts)
+        -- Hoogle search for the type signature of the definition under the cursor
+        vim.keymap.set('n', '<space>si', ht.hoogle.hoogle_signature, opts)
+        -- Evaluate all code snippets
+        vim.keymap.set('n', '<space>ea', ht.lsp.buf_eval_all, opts)
+        -- Toggle a GHCi repl for the current package
+        vim.keymap.set('n', '<leader>rr', ht.repl.toggle, opts)
+        -- Toggle a GHCi repl for the current buffer
+        vim.keymap.set('n', '<leader>rf', function()
+          ht.repl.toggle(vim.api.nvim_buf_get_name(0))
+        end, opts)
+        vim.keymap.set('n', '<leader>rq', ht.repl.quit, opts)
+      '';
     };
   };
 }
