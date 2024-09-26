319dcf1e65a4a27dd9da4a166265e211be6f69cc
Alexander Sauceda <alexander@alexandersauceda.dev>
2024-09-26 16:11:48 -0700

    fix(config/basic-plugins): explicitly enabling plugins.web-devicons

--- config/basic-plugins.nix
+++ config/basic-plugins.nix
@@ -5,6 +5,7 @@
     guess-indent.enable = true;
     tmux-navigator.enable = true;
     todo-comments.enable = true;
+    web-devicons.enable = true;
     # TODO: configure which-key: https://github.com/folke/which-key.nvim
     # nixvim docs: https://nix-community.github.io/nixvim/#_plugins_which_key_enable
     which-key.enable = true;
