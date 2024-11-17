dc1702a2f58e57679477a062ecd25990f098805c
Alexander <alexander@alexandersauceda.dev>
2024-11-17 00:37:17 -0800

    feat(config/lang/java): Add debug and test bundles (#372)
    * feat(config/lang/java): Adding debug and test bundles to Java config
    
    * Updating name for root dir for config, moving bemol ws folders function to one spot in LSP.
    
    * Commenting out workspace folders to see if that fixes the issue with non-project file
    
    * Changing naming of root dir and ws_folders attributes
    
    * Trying to use vim fs root and get rid of separate ws root functions and just one in the lsp file
    
    * Moving extra config lua post into top-level config block
    
    * Removing workspace folders entry and adding filtered types to completion options

--- config/lang/java.nix
+++ config/lang/java.nix
@@ -4,28 +4,39 @@
   helpers,
   ...
 }: {
-  # TODO: add debug adapter support
-  extraPackages = with pkgs; [
-    jdt-language-server
-  ];
+  extraPackages = with pkgs;
+    [
+      jdt-language-server
+    ]
+    ++ (with vscode-extensions.vscjava; [
+      vscode-java-debug
+      vscode-java-test
+    ]);
 
-  extraConfigLuaPre = ''
-    local jdtls_setup = require("jdtls.setup")
-    _M.root_dir = jdtls_setup.find_root({ "packageInfo" }, "Config")
-    _M.ws_folders_jdtls = {}
-    if _M.root_dir then
-     local file = io.open(_M.root_dir .. "/.bemol/ws_root_folders")
-     if file then
-      for line in file:lines() do
-       table.insert(_M.ws_folders_jdtls, "file://" .. line)
-      end
-      file:close()
-     end
-    end
+  extraConfigLuaPre = let
+    java-debug = "${pkgs.vscode-extensions.vscjava.vscode-java-debug}/share/vscode/extensions/vscjava.vscode-java-debug/server";
+    java-test = "${pkgs.vscode-extensions.vscjava.vscode-java-test}/share/vscode/extensions/vscjava.vscode-java-test/server";
+  in ''
+    local jdtls = require("jdtls")
+    _M.jdtls = {}
 
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
     local client_capabilities = vim.lsp.protocol.make_client_capabilities()
     _M.capabilities = cmp_nvim_lsp.default_capabilities(client_capabilities)
+
+    local jdtls_dap = require("jdtls.dap")
+
+    _M.jdtls.bundles = {}
+    local java_debug_bundle = vim.split(vim.fn.glob("${java-debug}" .. "/*.jar"), "\n")
+    local java_test_bundle = vim.split(vim.fn.glob("${java-test}" .. "/*.jar", true), "\n")
+    -- add jars to the bundle list if there are any
+    if java_debug_bundle[1] ~= "" then
+        vim.list_extend(_M.jdtls.bundles, java_debug_bundle)
+    end
+
+    if java_test_bundle[1] ~= "" then
+        vim.list_extend(_M.jdtls.bundles, java_test_bundle)
+    end
   '';
 
   autoCmd = [
@@ -48,23 +59,36 @@
 
   plugins.nvim-jdtls = {
     enable = true;
-    rootDir = helpers.mkRaw "_M.root_dir";
+    rootDir = helpers.mkRaw "_M.workspace_root_dir";
     cmd = [
       (lib.getExe pkgs.jdt-language-server)
       "-Dlog.protocol=true"
       "-Dlog.level=ALL"
       "--add-modules=ALL-SYSTEM"
     ];
-    data.__raw = "os.getenv('HOME') .. '/.local/share/eclipse/' .. vim.fn.fnamemodifyal(_M.root_dir, ':p:h:t')";
+    data.__raw = "os.getenv('HOME') .. '/.local/share/eclipse/' .. vim.fn.fnamemodify(_M.workspace_root_dir, ':p:h:t')";
     settings = {
       java = {
         signatureHelp.enable = true;
-        completion.enable = true;
+        completion = {
+          enable = true;
+          filteredTypes = [
+            "com.sun.*"
+            "io.micrometer.shaded.*"
+            "java.awt.*"
+            "jdk.*"
+            "sun.*"
+            "are.you.sure.*"
+          ];
+        };
         saveActions.organizeImports = true;
         implementationsCodeLens.enable = true;
         referenceCodeLens.enable = true;
         inlayHints = {
           parameterNames.enabled = "all";
+        };
+        codeGeneration = {
+          generateComments = true;
         };
       };
     };
@@ -72,7 +96,7 @@
       capabilities = helpers.mkRaw "_M.capabilities";
     };
     initOptions = {
-      workspaceFolders = helpers.mkRaw "_M.ws_folders_jdtls";
+      bundles = helpers.mkRaw "_M.jdtls.bundles";
     };
   };
 }
