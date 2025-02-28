bca81b0ba4fb0f1aafdc6baa711dd815705636de
Alexander <alexander@alexandersauceda.dev>
2025-02-27 21:55:45 -0700

    refactor: Updating bemol function and moving to lsp on attach. creating a new on attach function for jdtls (#401)
    * refactor(lang/java): Remove copyright header autocommand
    
    * fix(lang/java): Remove reordering imports on save
    
    * feat(lang/java): Updating how jdtls configures the data directory
    
    * test: Removing capabilities and removing extra packages from nvim-jdtls config to see if that fixes the debug support
    
    * test: Adding missing jdtls to module
    
    * test: Adding ignore bundle filter before adding bundles
    
    * test: Adding specific runtimes to jdt to see if that does the trick for the debugging server since that seems to be the gradle build issue
    
    * test: Adding the filtered bundles to jdtls bundles
    
    * test: changing jdtls setup from using cmd to data and configuration
    
    * refactor(config/lsp): Refactor bemol function to not use modules and contain function locally
    
    * refactor(config/lsp): Moving bemol function to on attach
    
    * test: Adding new on_attach function to jdtls
    
    * fix: making the jdtls onattach function raw lua
    
    * test: Adding debug log levels for lsp
    
    ---------
    
    Co-authored-by: Alexander Sauceda <alesauce@amazon.com>

--- config/lang/java.nix
+++ config/lang/java.nix
@@ -1,74 +1,47 @@
 {
   pkgs,
-  lib,
   helpers,
   ...
 }: {
-  extraPackages = with pkgs;
-    [
-      jdt-language-server
-    ]
-    ++ (with vscode-extensions.vscjava; [
-      vscode-java-debug
-      vscode-java-test
-    ]);
-
   extraConfigLuaPre = let
     java-debug = "${pkgs.vscode-extensions.vscjava.vscode-java-debug}/share/vscode/extensions/vscjava.vscode-java-debug/server";
     java-test = "${pkgs.vscode-extensions.vscjava.vscode-java-test}/share/vscode/extensions/vscjava.vscode-java-test/server";
   in ''
-    local jdtls = require("jdtls")
-    _M.jdtls = {}
-
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-    local client_capabilities = vim.lsp.protocol.make_client_capabilities()
-    _M.capabilities = cmp_nvim_lsp.default_capabilities(client_capabilities)
-
     local jdtls_dap = require("jdtls.dap")
 
+    _M.jdtls = {}
     _M.jdtls.bundles = {}
     local java_debug_bundle = vim.split(vim.fn.glob("${java-debug}" .. "/*.jar"), "\n")
     local java_test_bundle = vim.split(vim.fn.glob("${java-test}" .. "/*.jar", true), "\n")
-    -- add jars to the bundle list if there are any
-    if java_debug_bundle[1] ~= "" then
-        vim.list_extend(_M.jdtls.bundles, java_debug_bundle)
-    end
 
-    if java_test_bundle[1] ~= "" then
-        vim.list_extend(_M.jdtls.bundles, java_test_bundle)
-    end
-  '';
-
-  autoCmd = [
-    {
-      event = "BufNewFile";
-      pattern = "*.java";
-      callback.__raw = ''
-        function()
-          local header = {
-            "/*",
-            " * Copyright " .. os.date("%Y") .. " Amazon.com, Inc. or its affiliates. All Rights Reserved.",
-            " * All rights reserved.",
-            " */",
-          }
-          vim.api.nvim_buf_set_lines(0, 0, 0, false, header)
+    -- Following filters out unwanted bundles
+    local ignored_bundles = { "com.microsoft.java.test.runner-jar-with-dependencies.jar", "jacocoagent.jar" }
+    local find = string.find
+    local function should_ignore_bundle(bundle)
+        for _, ignored in ipairs(ignored_bundles) do
+            if find(bundle, ignored, 1, true) then
+                return true
+            end
         end
-      '';
-    }
-  ];
+    end
+    local filtered_java_debug_bundle = vim.tbl_filter(function(bundle) return bundle ~= "" and not should_ignore_bundle(bundle) end, java_debug_bundle)
+    local filtered_java_test_bundle = vim.tbl_filter(function(bundle) return bundle ~= "" and not should_ignore_bundle(bundle) end, java_test_bundle)
+    vim.list_extend(_M.jdtls.bundles, filtered_java_debug_bundle)
+    vim.list_extend(_M.jdtls.bundles, filtered_java_test_bundle)
+  '';
 
   plugins.nvim-jdtls = {
     enable = true;
-    rootDir = helpers.mkRaw "_M.workspace_root_dir";
-    cmd = [
-      (lib.getExe pkgs.jdt-language-server)
-      "-Dlog.protocol=true"
-      "-Dlog.level=ALL"
-      "--add-modules=ALL-SYSTEM"
-    ];
-    data.__raw = "os.getenv('HOME') .. '/.local/share/eclipse/' .. vim.fn.fnamemodify(_M.workspace_root_dir, ':p:h:t')";
+    configuration.__raw = ''vim.fn.stdpath 'cache' .. "/jdtls/config"'';
+    data = helpers.mkRaw "os.getenv('HOME') .. '/.local/share/eclipse' .. vim.fn.fnamemodify(vim.fs.root(0, '.git'), ':p:h:t')";
     settings = {
       java = {
+        configuration = {
+          runtimes = [
+            pkgs.jdk
+            pkgs.jdk17
+          ];
+        };
         signatureHelp.enable = true;
         completion = {
           enable = true;
@@ -81,7 +54,6 @@
             "are.you.sure.*"
           ];
         };
-        saveActions.organizeImports = true;
         implementationsCodeLens.enable = true;
         referenceCodeLens.enable = true;
         inlayHints = {
@@ -92,11 +64,18 @@
         };
       };
     };
-    extraOptions = {
-      capabilities = helpers.mkRaw "_M.capabilities";
-    };
     initOptions = {
       bundles = helpers.mkRaw "_M.jdtls.bundles";
+    };
+    extraOptions = {
+      onAttach = helpers.mkRaw ''
+        function(client, bufnr)
+          vim.lsp.set_log_level("debug")
+          jdtls.setup_dap({hotcodereplace = 'auto'})
+          jdtls.setup.add_commands()
+          bemol()
+        end
+      '';
     };
   };
 }
