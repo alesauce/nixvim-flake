a13236fd17975367e1245918104376f6b72f0b22
Alexander Sauceda <alexander@alexandersauceda.dev>
2025-04-30 18:02:14 -0600

    feat(config/lang/kotlin): Add inlay hints to kotlin language server config

--- config/lang/kotlin.nix
+++ config/lang/kotlin.nix
@@ -2,6 +2,18 @@
   plugins = {
     lsp.servers.kotlin_language_server = {
       enable = true;
+      settings = {
+        kotlin = {
+          diagnostics = {
+            level = "Information";
+          };
+          inlayHints = {
+            typeHints = true;
+            parameterHints = true;
+            chainedHints = true;
+          };
+        };
+      };
     };
   };
 }
