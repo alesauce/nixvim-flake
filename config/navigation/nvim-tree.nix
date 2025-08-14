b30f8fea7f5b26da9b82eb694591c20ce5daa1b2
Alexander Sauceda <alexander@alexandersauceda.dev>
2025-08-13 20:35:47 -0600

    chore(config/navigation/nvim-tree): Update deprecated attribute names

--- config/navigation/nvim-tree.nix
+++ config/navigation/nvim-tree.nix
@@ -1,13 +1,14 @@
 {
   plugins.nvim-tree = {
     enable = true;
-    disableNetrw = true;
-    autoReloadOnWrite = true;
-    hijackNetrw = true;
-    hijackUnnamedBufferWhenOpening = true;
-    sortBy = "case_sensitive";
-
-    filesystemWatchers.enable = true;
+    settings = {
+      disable_netrw = true;
+      hijack_netrw = true;
+      auto_reload_on_write = true;
+      hijack_unnamed_buffer_when_opening = true;
+      sort_by = "case_sensitive";
+      filesystem_watchers.enable = true;
+    };
   };
   keymaps = [
     {
